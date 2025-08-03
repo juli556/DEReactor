148cca7d39d4101a3061865bb552648eb23ea846
ne_ju <44039556+juli556@users.noreply.github.com>
2025-08-04 00:11:13 +0200

    Update install.lua
    update urls

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --
 --
 
-local libURL = "https://raw.githubusercontent.com/coaber/DEReactor/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/coaber/DEReactor/master/drmon.lua"
+local libURL = "https://raw.githubusercontent.com/juli556/DEReactor/refs/heads/master/drmon.lua"
+local startupURL = "https://raw.githubusercontent.com/juli556/DEReactor/refs/heads/master/drmon.lua"
 local lib, startup
 local libFile, startupFile
 
